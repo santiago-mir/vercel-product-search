f58aed295ea264d8e7cf6dc92de4e0aa367963b0
Santiago <san_chan97@hotmail.com>
2025-01-10 16:40:03 -0300

    update endpoint products

--- lib/requests.ts
+++ lib/requests.ts
@@ -3,9 +3,16 @@ import type { NextApiRequest, NextApiResponse } from "next";
 export function getLimitAndOffset(req: NextApiRequest, MaxLimit, MaxOffset) {
   const queryLimit = parseInt(req.query.limit as string);
   const queryOffset = parseInt(req.query.offset as string);
-
-  const limit = queryLimit <= MaxLimit && queryLimit > 0 ? queryLimit : 100;
-  const offset = queryOffset <= MaxOffset ? queryOffset : 0;
+  const limit = queryLimit
+    ? queryLimit <= MaxLimit
+      ? queryLimit
+      : MaxLimit
+    : 10;
+  const offset = queryOffset
+    ? queryOffset <= MaxOffset
+      ? queryOffset
+      : MaxOffset
+    : 0;
   return {
     limit,
     offset,
--- pages/api/products/index.ts
+++ pages/api/products/index.ts
@@ -1,12 +1,29 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 import { getLimitAndOffset } from "lib/requests";
-import { airtableBase } from "lib/airtable";
+import methods from "micro-method-router";
 import { client } from "lib/algolia";
 
-export default async function (req: NextApiRequest, res: NextApiResponse) {
-  const response = await client.searchSingleIndex({
-    indexName: "products",
-    searchParams: { query: req.query.search as string },
-  });
-  res.send(response);
-}
+export default methods({
+  async get(req: NextApiRequest, res: NextApiResponse) {
+    const { limit, offset } = getLimitAndOffset(req, 100, 19);
+    const query = req.query.q as string;
+    const algoliaOffset = Math.floor(offset / limit);
+    const response = await client.searchSingleIndex({
+      indexName: "products",
+      searchParams: {
+        query,
+        hitsPerPage: limit,
+        page: algoliaOffset,
+      },
+    });
+    res.send({
+      results: response.hits,
+      pagination: {
+        offset,
+        limit,
+        page: response.page,
+        total: response.nbHits,
+      },
+    });
+  },
+});
